7cd0500247f86a12011184eca9366ddbc33454b6
unknown <abusiddick84@gmail.com>
2024-04-28 17:11:14 +0600

    Send data to database successfully

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const artCollection = client.db('artDB').collection('artAndCraft')
+
+    app.post('/artCraft', async(req, res)=>{
+      const newArtCraft = req.body;
+      console.log(newArtCraft);
+      const result = await artCollection.insertOne(newArtCraft);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
